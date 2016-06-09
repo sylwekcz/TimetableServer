6fcdda710dab8027e551f96900d66a63959fba56
sylwek <sylwekczmil@gmail.com>
2016-06-09 13:19:59 +0200

    Refactor authentication

--- src/main/java/pl/sylwekczmil/timetableserver/controller/AuthenticationFilter.java
+++ src/main/java/pl/sylwekczmil/timetableserver/controller/AuthenticationFilter.java
@@ -20,27 +20,6 @@ import javax.ws.rs.ext.Provider;
 import pl.sylwekczmil.timetableserver.Secured;
 import pl.sylwekczmil.timetableserver.User;
 
-/*Inject a proxy of the SecurityContext in any REST endpoint class:
-
-    @Context
-    SecurityContext securityContext;
-The same can be done in a method:
-
-    @GET
-    @Secured
-    @Path("{id}")
-    @Produces("application/json")
-    public Response myMethod(@PathParam("id") Long id, 
-                             @Context SecurityContext securityContext) {
-        ...
-    }
-And get the Principal:
-
-    Principal principal = securityContext.getUserPrincipal();
-    String username = principal.getName();*/
-
-
-
 @Secured
 @Provider
 @Priority(Priorities.AUTHENTICATION)
@@ -65,21 +44,17 @@ public class AuthenticationFilter implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
 
-        // Get the HTTP Authorization header from the request
         String authorizationHeader
                 = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 
-        // Check if the HTTP Authorization header is present and formatted correctly 
         if (authorizationHeader == null) {
             throw new NotAuthorizedException("Authorization header must be provided");
         }
 
-        // Extract the token from the HTTP Authorization header
         String token = authorizationHeader;
 
         try {
-
-            // Validate the token
+        
             validateToken(token);
 
         } catch (Exception e) {
